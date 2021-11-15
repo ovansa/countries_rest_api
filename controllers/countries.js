e6bb3478e45e600fa5f4dfb0bc501ed301c8e23a
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2021-11-15 08:20:20 +0100

    Added custom error messages, added asynchandler to refactor code

--- controllers/countries.js
+++ controllers/countries.js
@@ -1,38 +1,29 @@
 const Country = require('../models/Country');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc Get all countries
 // @route GET /api/v1/countries
 // @access Public
-export const getCountries = async (req, res, next) => {
-  try {
-    const countries = await Country.find();
-    res.status(200).json({ success: true, data: countries });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+export const getCountries = asyncHandler(async (req, res, next) => {
+  const countries = await Country.find();
+  res.status(200).json({ success: true, data: countries });
+});
 
 // @desc Get single country
 // @route GET /api/v1/countries/:id
 // @access Public
-export const getCountry = async (req, res, next) => {
-  try {
-    const country = await Country.findById(req.params.id);
+export const getCountry = asyncHandler(async (req, res, next) => {
+  const country = await Country.findById(req.params.id);
 
-    if (!country) {
-      next(
-        new ErrorResponse(`Country not found with id of ${req.params.id}`, 404)
-      );
-    } else {
-      res.status(200).json({ success: true, data: country });
-    }
-  } catch (err) {
+  if (!country) {
     next(
       new ErrorResponse(`Country not found with id of ${req.params.id}`, 404)
     );
+  } else {
+    res.status(200).json({ success: true, data: country });
   }
-};
+});
 
 // @desc Create countries
 // @route POST /api/v1/countries
@@ -43,15 +34,25 @@ export const createCountries = async (req, res, next) => {
 
     res.status(201).json({ success: true, data: country });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
 // @desc Delete country
 // @route DELETE /api/v1/countries/:id
 // @access Public
-export const deleteCountry = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: 'true', msg: `Delete country ${req.params.id}` });
+export const deleteCountry = async (req, res, next) => {
+  try {
+    const country = await Country.findByIdAndDelete(req.params.id);
+
+    if (!country) {
+      next(
+        new ErrorResponse(`Country not found with id of ${req.params.id}`, 404)
+      );
+    } else {
+      res.status(200).json({ success: true, data: {} });
+    }
+  } catch (err) {
+    next(err);
+  }
 };
