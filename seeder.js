cabbed836bb16f2173ec8c2db7a8eaf7cd281b94
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2021-11-21 17:44:44 +0100

    Added seeder feature to populate db with sample data

---
+++ seeder.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+const Country = require('./models/Country');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const countries = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/countries.json`, 'utf8')
+);
+
+const importData = async () => {
+  try {
+    await Country.create(countries);
+
+    console.log('Countries imported'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Country.deleteMany();
+
+    console.log('Countries Destroyed'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
